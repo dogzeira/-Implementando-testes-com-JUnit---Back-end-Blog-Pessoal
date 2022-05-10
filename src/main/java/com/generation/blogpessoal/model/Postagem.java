7a35a298224f60141f3b5cbd8614d4fa1995ca3e
dogzeira <douglasdanilobutu13@gmail.com>
2022-05-10 19:08:49 -0300

    Atualizou As Classes Incrementando O Security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -13,59 +14,82 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table (name = "tb_postagem")
+@Table(name = "tb_postagem")
 
 public class Postagem {
-                                       
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
 
- @NotBlank (message = "O atributo títlo é obrigatório !!")
- @Size (min = 5, max = 100, 
- message = "O atributo títlo deve conter no min 5 e no max 100 carácteres!!") 
- private String titulo;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
- @NotNull (message = "O atributo texto é obrigatório !!")
- @Size (min = 10, max = 1000, 
- message = "O atributo texto deve conter no min 5 e no max 100 carácteres!!")
- private String texto;
+	@NotBlank(message = "O atributo títlo é obrigatório !!")
+	@Size(min = 5, max = 100, message = "O atributo títlo deve conter no min 5 e no max 100 carácteres!!")
+	private String titulo;
 
- @UpdateTimestamp 
- private LocalDateTime data;
-    
+	@NotNull(message = "O atributo texto é obrigatório !!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no min 5 e no max 100 carácteres!!")
+	private String texto;
+
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 	public Long getId() {
-	return id;
-}
+		return id;
+	}
 
-    public void setId(Long id) {
-	this.id = id;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitulo() {
-	return titulo;
-}
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public void setTitulo(String titulo) {
-	this.titulo = titulo;
-}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public String getTexto() {
-	return texto;
-}
+	public String getTexto() {
+		return texto;
+	}
 
-    public void setTexto(String texto) {
-	this.texto = texto;
-}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
 
-    public LocalDateTime getData() {
-	return data;
-}
+	public LocalDateTime getData() {
+		return data;
+	}
 
-    public void setData(LocalDateTime data) {
-	this.data = data;
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
 }
-}
-
 
